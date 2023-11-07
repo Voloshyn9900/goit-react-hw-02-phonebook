b0e0c20ff07d438ea6a15728fa4fa76ff2888f4b
Yurii Voloshyn <voloshyn9900@gmail.com>
2023-11-07 06:53:02 +0100

    commit1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,13 @@
+import { Feedback } from './Feedback';
+import { Section, Container, Title } from './App.styled';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Section>
+      <Container>
+        <Title>Hello</Title>
+        <Feedback></Feedback >
+      </Container>
+    </Section>
   );
 };
